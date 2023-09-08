95c5d1bd793305cce0f340698ad179aa43e00606
Gaurav <gauravsyr10@gmail.com>
2023-09-08 12:13:36 +0530

    first commit

---
+++ Array/largest_element_in_array.py
@@ -0,0 +1,24 @@
+# Input: arr[] = {10, 20, 4}
+# Output: 20
+
+# Input: arr[] = {20, 10, 20, 4, 100}
+# Output: 100
+
+
+def largest(arr, n):
+
+    maxi = arr[0]
+
+    for i in range(1, n):
+        if arr[i] > maxi:
+            maxi = arr[i]
+    return maxi
+
+
+if __name__ == '__main__':
+    arr = [10, 324, 45, 90, 9808]
+    n = len(arr)
+
+    Ans = largest(arr, n)
+
+    print("Largest in given array is", Ans)
